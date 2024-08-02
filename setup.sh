fd9c6c8b0e8ded2afaccf4886665a1fe2182afe8
loop <47750178+DaniloKrkeljic@users.noreply.github.com>
2024-08-02 21:16:47 +0200

    removed geth run from setup.sh

--- setup.sh
+++ setup.sh
@@ -24,10 +24,9 @@ cd ..
 
 # Generate the genesis file using prysmctl
 ./prysmctl testnet generate-genesis --fork deneb --num-validators 64 --genesis-time-delay 90 --chain-config-file config.yml --geth-genesis-json-in genesis.json  --geth-genesis-json-out genesis.json --output-ssz genesis.ssz
+
 # Call the geth command and import the account with the password "yay"
 echo -e "yay\nyay" | ./geth --datadir=gethdata account import secret.txt
 
 # Initialize the datadir with the genesis file
 ./geth --datadir=gethdata init genesis.json
-
-echo "yay" | ./geth --http --http.api eth,net,web3 --ws --ws.api eth,net,web3 --authrpc.jwtsecret jwt.hex --datadir gethdata --syncmode full --allow-insecure-unlock --unlock 0x123463a4b065722e99115d6c222f267d9cabb524 --bootnodes "enode://1eb5d4f39ab4da4d95f553d242163e5cf5dad35faf704c294759f87d4cad592e861d5a9572243608814e34ed410af51e554030d1cdc4ecf829aef6d0b1aa9988@172.81.179.112:30303"
